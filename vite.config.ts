18b13279ad4f510944d36afafdba91ee46b95b8a
gjy <1039062164@qq.com>
2025-09-29 18:57:50 +0800

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/online-resume/",
   plugins: [vue()],
   resolve: {
     // 配置路径别名
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
